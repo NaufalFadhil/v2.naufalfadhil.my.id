447958b575744eba4f2d3041332e6ac912d89a44
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2026-04-29 20:08:52 +0700

    feat(blog): add new post on Laravel project structure with supporting images

--- src/generated/image-manifest.ts
+++ src/generated/image-manifest.ts
@@ -44,6 +44,17 @@ export const imageManifest: Record<string, { widths: number[]; base: string; ext
     "base": "/blogs/2024-06-premium-tier-and-standard-tier-gcloud/standard-network-tier",
     "ext": ".png"
   },
+  "/blogs/2024-11-best-laravel-project-structure/cover.png": {
+    "widths": [
+      480,
+      768,
+      960,
+      1440,
+      1536
+    ],
+    "base": "/blogs/2024-11-best-laravel-project-structure/cover",
+    "ext": ".png"
+  },
   "/blogs/2026-04-stock-market-analysis-story/cover.png": {
     "widths": [
       480,
